0c71c4f7d82448167614ccc7a0cc87019af7603a
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-02-27 15:42:40 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,6 +1,5 @@
 function adduser(){
     user_name=document.getElementById("user_name").value;
-localStorage.setItem("user_name",user_name);
-window.location="kwitter_room.html";
+    localStorage.setItem("user_name",username);
+    window.location="kwitter_game.html";
 }
-
