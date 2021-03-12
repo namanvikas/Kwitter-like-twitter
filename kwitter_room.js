87697131458f27836b20db18da6aa8713197d3b2
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-03-12 16:14:10 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,3 +11,36 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+  function addroom(){
+    room_name=document.getElementById("room_name").value;
+    firebase.database().ref("/").child(room_name).update({
+          purpose:"adding room name"
+          
+          
+    })
+;
+
+  localStorage.setItem("room_name",room_name);
+  window.location="kwitter_room.js"
+  }
+  function data(){
+    firebase.database().ref("/").on('value'),
+     function(snapshot) {
+       document.getElementById("output").innerHTML = "";
+       snapshot.forEach(
+         function(childSnapshot) {
+           childKey  = childSnapshot.key;
+  }
+document.getElementById(output).innerHTML+="row";
+getData()
+
+function redirectToRoomName(name){
+  localStorage.setitem("room_name",room_name)
+  window.location="kwitter_page.html";
+}
+function logout(){
+  localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
+}
